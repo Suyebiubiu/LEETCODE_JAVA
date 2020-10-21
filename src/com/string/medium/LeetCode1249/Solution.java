f3f8c6d3b5e3297e98ea5b4b65e040823fac4fa0
Suyebiubiu <940158620@qq.com>
2020-10-21 23:26:25 +0800

    '2020.10.21'

--- src/com/string/medium/LeetCode1249/Solution.java
+++ src/com/string/medium/LeetCode1249/Solution.java
@@ -1,13 +1,14 @@
-package com.string.medium;
+package com.string.medium.LeetCode1249;
 
 /**
  * @author GMT
  * @version 1.0
  * @create 2020-10-2022:40
  */
-public class LeetCode1249 {
+public class Solution {
 
     public String minRemoveToMakeValid(String s) {
+
         return null;
     }
 
