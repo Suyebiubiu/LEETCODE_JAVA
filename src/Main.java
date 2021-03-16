cd61e97afa7294057caf8563ccd1cdb01a5936a3
Suyebiubiu <940158620@qq.com>
2021-03-16 14:30:17 +0800

    '20210316'

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,37 @@
+import com.sun.org.apache.regexp.internal.RE;
+
 public class Main {
     public boolean exist(char[][] board, String word) {
         char[] words = word.toCharArray();
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (dfs(board, words, i, j, 0)) return true;
+                if (dfs(board, words, i, j, 0))
+                    return true;
             }
         }
+
+
         return false;
     }
 
-    boolean dfs(char[][] board, char[] words, int i, int j, int k) {
+    public boolean dfs(char[][] board, char[] words, int i, int j, int k) {
         if (i < 0 || i > board.length - 1 || j < 0 || j > board[0].length - 1) return false;
         if (board[i][j] != words[k]) return false;
-
         if (words.length - 1 == k) return true;
-        board[i][j] = '\0';
 
-        boolean res = dfs(board, words, i - 1, j, k + 1)
-                || dfs(board, words, i + 1, j, k + 1)
-                || dfs(board, words, i, j - 1, k + 1)
-                || dfs(board, words, i, j + 1, k + 1);
+        board[i][j] = '\0';
+        boolean res = dfs(board, words, i - 1, j, k + 1) || dfs(board, words, i + 1, j, k + 1)
+                || dfs(board, words, i, j - 1, k + 1) || dfs(board, words, i, j + 1, k + 1);
         board[i][j] = words[k];
         return res;
     }
 
     public static void main(String[] args) {
-        char[][] arr = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+        char[][] arr = {
+                {'A', 'B', 'C', 'E'},
+                {'S', 'F', 'C', 'S'},
+                {'A', 'D', 'E', 'E'}};
         String word = "ABCCED";
         System.out.println(new Main().exist(arr, word));
     }
-
 }
---
+++ src/cn/swordOffer/num13/RobotMoveRange.java
@@ -0,0 +1,10 @@
+package cn.swordOffer.num13;
+
+/**
+ * @author GONG
+ * @version 1.0
+ * @date 2021/3/16 14:29
+ */
+public class RobotMoveRange {
+
+}
